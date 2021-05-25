e3b602a3fd67ccb1c4b99b480f5a4d87558f1ad2
Dhanushya1234 <dhanushyaedu@gmail.com>
2021-05-25 09:15:09 -0700

    commit

--- StudentInfo/src/studentInfo/StudCreate.java
+++ StudentInfo/src/studentInfo/StudCreate.java
@@ -5,10 +5,37 @@
  */
 package studentInfo;
 
+import java.util.Scanner;
+import studentBo.StudentBo;
+
 /**
  *
  * @author lenovo
  */
 public class StudCreate {
-    
+     String name;   
+     String gender;
+     String dob;
+     int age;
+     String address;
+     int mobileNo;
+     StudentBo StudCreate(){
+         Scanner in=new Scanner(System.in);
+         System.out.println("Enter Your Name");
+         name=in.next();
+         System.out.println("Enter Your Gender");
+         gender=in.next();
+         System.out.println("Enter Your Date Of Birth");
+         dob=in.next();
+         System.out.println("Enter Your Age");
+         age=in.nextInt();
+         System.out.println("Enter Your Address");
+         address=in.next();
+         System.out.println("Enter Your Mobile Number");
+         mobileNo=in.nextInt();
+         
+         
+         StudentBo student=new StudentBo(name,gender,dob,age,address,mobileNo);
+         return student;
+     }
 }
